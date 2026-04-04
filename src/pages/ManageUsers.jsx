c7054a9c9a4158733b79af80ddab0aafa38b85da
naufal <syahrultsaqib@gmail.com>
2026-04-04 18:32:58 +0700

    menambah modal form untuk menambah dan mengedit user

--- src/pages/ManageUsers.jsx
+++ src/pages/ManageUsers.jsx
@@ -181,8 +181,9 @@ export default function ManageUsers() {
                           <button
                             className="btn-action"
                             onClick={() => deleteUser(u.id)}
+                            style={{borderColor: 'red'}}
                           >
-                            <Trash2 size={16} />
+                            <Trash2 size={16} style={{color: 'red'}}/>
                           </button>
                         </div>
                       </td>
@@ -193,6 +194,130 @@ export default function ManageUsers() {
             </div>
           </div>
         </main>
+
+        {showModal && (
+          <div className="modal-overlay">
+            <div className="modal-box">
+              <div className="modal-header" style={{ marginBottom: "20px", display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                <h2>{editId ? "Edit User" : "Tambah User"}</h2>
+                <button type="button" onClick={() => setShowModal(false)} style={{ background: "transparent", border: "none", cursor: "pointer" }}>
+                  <X size={24} color="#64748b" />
+                </button>
+              </div>
+
+              <form
+                className="modal-form"
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  saveUser();
+                }}
+              >
+                <div className="form-row">
+                  <div className="form-col">
+                    <label htmlFor="nama" style={{margin: '0 0 0.5rem 0.5rem'}}>Nama</label>
+                    <input
+                      name="nama"
+                      placeholder="Nama Lengkap"
+                      value={form.nama}
+                      onChange={handleChange}
+                      autoFocus
+                      />
+                  </div>
+                  <div className="form-col">
+                    <label htmlFor="username" style={{margin: '0 0 0.5rem 0.5rem'}}>username</label>
+                    <input
+                      name="username"
+                      placeholder="Username"
+                      value={form.username}
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
+
+                <div className="form-row">
+                  <div className="form-col">
+                  <label htmlFor="password" style={{margin: '0 0 0.5rem 0.5rem'}}>password</label>
+                  <input
+                    name="password"
+                    placeholder="Password (Isi baru/ubah)"
+                    value={form.password}
+                    onChange={handleChange}
+                    />
+                  </div>
+                  <div className="form-col">
+                  <label htmlFor="role" style={{margin: '0 0 0.5rem 0.5rem'}}>role</label>
+                  <select name="role" value={form.role} onChange={handleChange}>
+                    <option value="user">User</option>
+                    <option value="admin">Admin</option>
+                    <option value="superadmin">Super Admin</option>
+                  </select>
+                  </div>
+                </div>
+
+                <div className="form-row">
+                  <div className="form-col">
+                  <label htmlFor="ttl" style={{margin: '0 0 0.5rem 0.5rem'}}>tanggal lahir</label>
+                  <input
+                    type="date"
+                    name="ttl"
+                    value={form.ttl}
+                    onChange={handleChange}
+                    />
+                  </div>
+                  <div className="form-col">
+                  <label htmlFor="gender">Gender</label>
+                  <select name="gender" value={form.gender} onChange={handleChange}>
+                    <option value="Laki-laki">Laki-laki</option>
+                    <option value="Perempuan">Perempuan</option>
+                  </select>
+                  </div>
+                </div>
+
+                <div className="form-row">
+                  <div className="form-col">
+                  <label htmlFor="nohp" style={{margin: '0 0 0.5rem 0.5rem'}}>nomor hp</label>
+                  <input
+                    name="nohp"
+                    placeholder="Nomor HP"
+                    value={form.nohp}
+                    onChange={handleChange}
+                    />
+                  </div>
+                  <div className="form-col">
+                  <label htmlFor="status" style={{margin: '0 0 0.5rem 0.5rem'}}>status</label>
+                  <select name="status" value={form.status} onChange={handleChange}>
+                    <option value="aktif">Aktif</option>
+                    <option value="tidak aktif">Tidak Aktif</option>
+                  </select>
+                  </div>
+                </div>
+
+                <div className="form-row-full">
+                  <div className="form-col">
+                    <label htmlFor="alamat" style={{margin: '0 0 0.5rem 0.5rem'}}>
+                      alamat
+                    </label>
+                    <input
+                      name="alamat"
+                      placeholder="Alamat"
+                      value={form.alamat}
+                      onChange={handleChange}
+                      />
+                  </div>
+                </div>
+
+                <div className="modal-actions">
+                  <button type="button" className="btn-cancel" onClick={() => setShowModal(false)}>
+                    Batal
+                  </button>
+                  <button type="submit" className="btn-save">
+                    Simpan
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
